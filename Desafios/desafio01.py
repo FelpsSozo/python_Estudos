18a3d007265e38dc8268c9dc65beb650e2e68b7f
Felipe Sozo <felipesozo22@outlook.com>
2026-08-27 20:03:58 -0300

    Update desafio01.py
    Mudança no texto de boas vindas.

--- Desafios/desafio01.py
+++ Desafios/desafio01.py
@@ -1,4 +1,4 @@
 # Crie um script Python que leia o nome de uma pessoa e mostre a mensagem de boas-vindas de acordo com o valor digitado.
 
-nome = input('Qual é o seu nome?')
-print('Boas vindas', nome, '!')
+nome = input('Qual é o seu nome? ')
+print('Olá', nome, '! Prazer em te conhecer!')  
